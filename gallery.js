d2c8908a560ade70cc6b51e3e13b33c06e3cfd19
Karl-KD <karlanjelo.pastrana.iics@ust.edu.ph>
2020-02-16 11:47:45 +0800

    update

--- gallery.js
+++ gallery.js
@@ -1,8 +1,8 @@
 function upDate(previewPic){
-    var src = previewPic.getAttribute( "src" );
-    var alt = previewPic.getAttribute( "alt" );
-    document.getElementById('image').style.backgroundImage = "url('" + src + "')";
-    document.getElementById('image').innerHTML = alt;
+    var a = previewPic.getAttribute( "src" );
+    var b = previewPic.getAttribute( "alt" );
+    document.getElementById('image').style.backgroundImage = "url('" + a + "')";
+    document.getElementById('image').innerHTML = b;
  }
  
    function unDo(X){
